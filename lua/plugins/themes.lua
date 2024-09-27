02e8cbb441beabb4719868385a114c665b999641
gabrielwsa <gabrielwilliamsilvadearaujo@gmail.com>
2024-09-27 16:33:51 -0400

    kanagawa theme config

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,17 +2,18 @@ return {
   {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000,
+--[[ priority = 1000,
     config = function()
       vim.cmd.colorscheme "catppuccin"
     end
+]]
   },
 
   {
-    "rose-pine/neovim",
-    variant = "auto", -- auto, main, moon, or dawn
-    dark_variant = "main", -- main, moon, or dawn
-    dim_inactive_windows = false,
-    extend_background_behind_borders = true,
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "kanagawa"
+    end
   },
 }
